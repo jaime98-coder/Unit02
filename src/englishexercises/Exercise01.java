77e6019b426674e15a16250c1bce221b6a0fd223
jaime98-coder <jaime.martin@iesnervion.es>
2025-10-24 13:43:18 +0200

    commit for exercises 1 and 3

--- src/englishexercises/Exercise01.java
+++ src/englishexercises/Exercise01.java
@@ -1,12 +1,43 @@
 package englishexercises;
 
-
+import java.util.Scanner;
 
 public class Exercise01 {
 
 	public static void main(String[] args) {
+		final double SCREEN1_38 = 75.99;
+		final double SCREEN2_43 = 99.99;
+		final double AVSOFTWARE = 65.99;
+		final double PRINTER = 125.00;
+		int option;
+		double totalPrice=375.99;
+		String answer;
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Which screen do you prefer?");
+		System.out.println("1. 38cm -"+SCREEN1_38);
+		System.out.println("2. 43cm -"+SCREEN2_43);
+		option = reader.nextInt();
+		switch (option) {
+		case 1:
+			totalPrice+=SCREEN1_38;
+			break;
+		case 2:
+			totalPrice+=SCREEN2_43;
+			break;
+		}
+		System.out.println("Do you want an antivirus software? (Y/N)");
+		answer = reader.next();
+		if (answer.equalsIgnoreCase("Y")) {
+			totalPrice+=AVSOFTWARE;
+		}
+		System.out.println("Do you want a printer? (Y/N)");
+		answer = reader.next();
+		if (answer.equalsIgnoreCase("Y")) {
+			totalPrice+=PRINTER;
+		}
+		System.out.println(totalPrice);
+		reader.close();
 		
-
 	}
 
 }
---
+++ src/englishexercises/Exercise03.java
@@ -0,0 +1,46 @@
+package englishexercises;
+
+import java.util.Scanner;
+
+public class Exercise03 {
+
+	public static void main(String[] args) {
+		int mirrorHour;
+		int mirrorMinute;
+
+		int realHour;
+		int realMinute;
+		Scanner reader = new Scanner(System.in);
+
+		System.out.println("Introduce the hour that you are seeing in the mirror:");
+		mirrorHour = reader.nextInt();
+		mirrorMinute = reader.nextInt();
+
+		// if hour==12 I change it to 0 to operate later
+		mirrorHour = (mirrorHour == 12) ? 0 : mirrorHour;
+
+		// Hour oclock
+		if (mirrorMinute == 0) {
+			realHour = 12 - mirrorHour;
+			realMinute = 0;
+
+		} else {
+			// Not hour oclock
+			realHour = 11 - mirrorHour;
+			realMinute = 60 - mirrorMinute;
+		}
+
+		if (realHour < 10) {
+			System.out.print("0");
+		}
+		System.out.print(realHour + ":");
+
+		if (realMinute < 10) {
+			System.out.print("0");
+		}
+		System.out.println(realMinute);
+
+		reader.close();
+	}
+
+}
